c46434a04b4ed0c349446baec64f3745d29c2a93
aasma12345 <94236134+aasma12345@users.noreply.github.com>
2021-12-04 20:48:06 +0530

    Create e.py

---
+++ e.py
@@ -0,0 +1,94 @@
+i=10
+while i>=1:
+    print(i)
+    i=i-1
+
+
+i=105
+while i>=7:
+    print(i)
+    i=i-7
+
+
+
+i=1
+sum=0
+while i<=10:
+    sum=sum+i
+    i=i+1
+print(sum)
+
+
+i=1
+sum=0
+while i<=10:
+    sum=sum+i
+    print(sum)
+    i=i+1
+
+
+
+
+i=1
+sum=0
+while i<=20:
+    if i%2==0:
+        sum=sum+i
+    i=i+1
+print(sum)
+
+
+i=1
+sum=0
+while i<=20:
+    if i%2==0:
+       sum=sum+i
+       print(sum)
+    i=i+1
+
+
+num=int(input("enter the number: "))
+i=0
+while i<=num:
+    if num%2==0:
+        print("not prime number")
+    i=i+1
+else:
+    print("prime number")
+
+
+
+i=1
+while i<=1:
+    j=1
+    while j<=20:
+        print(j,"=",j*j)
+        j=j+1
+    print()
+    i=i+1
+
+
+
+num=int(input("enter the number---"))
+i=1
+while i<=num:
+    j=1
+    while j<=10:
+        print(i,"*",j,"=",i*j)
+        j=j+1
+    print()
+    i=i+1 
+
+
+i=11
+while i<=39:
+    j=11
+    while j<=39:
+        print(j,"=",j*j)
+        j=j+1
+    print()
+    i=i+1
+
+
+
+
